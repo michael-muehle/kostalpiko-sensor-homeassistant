148daef3646ca20f7968a95f3af93a71997bfba8
michael-muehle <97169252+michael-muehle@users.noreply.github.com>
2023-06-08 15:52:37 +0200

    Update const.py
    All Electrical Units changed to
    
    UnitOfPower.WATT
    UnitOfEnergy.KILO_WATT_HOUR,
    UnitOfElectricPotential.VOLT,
    UnitOfElectricCurrent.AMPERE

--- custom_components/kostal/const.py
+++ custom_components/kostal/const.py
@@ -2,10 +2,10 @@
 from datetime import timedelta
 
 from homeassistant.const import (
-    POWER_WATT,
-    ENERGY_KILO_WATT_HOUR,
+    UnitOfPower.WATT,
+    UnitOfEnergy.KILO_WATT_HOUR,
     UnitOfElectricPotential.VOLT,
-    ELECTRICAL_CURRENT_AMPERE,
+    UnitOfElectricCurrent.AMPERE,
 )
 
 DOMAIN = "kostal"
@@ -15,24 +15,24 @@ DEFAULT_NAME = "Kostal piko"
 MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=30)
 
 SENSOR_TYPES = {
-    "solar_generator_power": ["Solar generator power", POWER_WATT, "mdi:solar-power"],
-    "consumption_phase_1": ["Consumption phase 1", POWER_WATT, "mdi:power-socket-eu"],
-    "consumption_phase_2": ["Consumption phase 2", POWER_WATT, "mdi:power-socket-eu"],
-    "consumption_phase_3": ["Consumption phase 3", POWER_WATT, "mdi:power-socket-eu"],
-    "current_power": ["Current power", POWER_WATT, "mdi:solar-power"],
-    "total_energy": ["Total energy", ENERGY_KILO_WATT_HOUR, "mdi:solar-power"],
-    "daily_energy": ["Daily energy", ENERGY_KILO_WATT_HOUR, "mdi:solar-power"],
+    "solar_generator_power": ["Solar generator power", UnitOfPower.WATT, "mdi:solar-power"],
+    "consumption_phase_1": ["Consumption phase 1", UnitOfPower.WATT, "mdi:power-socket-eu"],
+    "consumption_phase_2": ["Consumption phase 2", UnitOfPower.WATT, "mdi:power-socket-eu"],
+    "consumption_phase_3": ["Consumption phase 3", UnitOfPower.WATT, "mdi:power-socket-eu"],
+    "current_power": ["Current power", UnitOfPower.WATT, "mdi:solar-power"],
+    "total_energy": ["Total energy", UnitOfEnergy.KILO_WATT_HOUR, "mdi:solar-power"],
+    "daily_energy": ["Daily energy", UnitOfEnergy.KILO_WATT_HOUR, "mdi:solar-power"],
     "string1_voltage": ["String 1 voltage", UnitOfElectricPotential.VOLT, "mdi:current-ac"],
-    "string1_current": ["String 1 current", ELECTRICAL_CURRENT_AMPERE, "mdi:flash"],
+    "string1_current": ["String 1 current", UnitOfElectricCurrent.AMPERE, "mdi:flash"],
     "string2_voltage": ["String 2 voltage", UnitOfElectricPotential.VOLT, "mdi:current-ac"],
-    "string2_current": ["String 2 current", ELECTRICAL_CURRENT_AMPERE, "mdi:flash"],
+    "string2_current": ["String 2 current", UnitOfElectricCurrent.AMPERE, "mdi:flash"],
     "string3_voltage": ["String 3 voltage", UnitOfElectricPotential.VOLT, "mdi:current-ac"],
-    "string3_current": ["String 3 current", ELECTRICAL_CURRENT_AMPERE, "mdi:flash"],
+    "string3_current": ["String 3 current", UnitOfElectricCurrent.AMPERE, "mdi:flash"],
     "l1_voltage": ["L1 voltage", UnitOfElectricPotential.VOLT, "mdi:current-ac"],
-    "l1_power": ["L1 power", POWER_WATT, "mdi:power-plug"],
+    "l1_power": ["L1 power", UnitOfPower.WATT, "mdi:power-plug"],
     "l2_voltage": ["L2 voltage", UnitOfElectricPotential.VOLT, "mdi:current-ac"],
-    "l2_power": ["L2 power", POWER_WATT, "mdi:power-plug"],
+    "l2_power": ["L2 power", UnitOfPower.WATT, "mdi:power-plug"],
     "l3_voltage": ["L3 voltage", UnitOfElectricPotential.VOLT, "mdi:current-ac"],
-    "l3_power": ["L3 power", POWER_WATT, "mdi:power-plug"],
+    "l3_power": ["L3 power", UnitOfPower.WATT, "mdi:power-plug"],
     "status": ["Status", None, "mdi:solar-power"],
 }
